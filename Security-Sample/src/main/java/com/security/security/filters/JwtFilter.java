ec9211aaea3be9fc7c9efe558b66300766f615d0
Surya <damasprakash@gmail.com>
2024-10-05 10:59:56 +0530

    Validate JWT token and pass Authentication object to UsernamePasswordAuthenticationFilter

--- Security-Sample/src/main/java/com/security/security/filters/JwtFilter.java
+++ Security-Sample/src/main/java/com/security/security/filters/JwtFilter.java
@@ -1,5 +1,13 @@
 package com.security.security.filters;
 
+import com.security.security.service.JwtService;
+import com.security.security.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +19,32 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    ApplicationContext context;
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String userName = null;
+        // get token and username from Authorization Header
+        if(authHeader!=null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            userName = jwtService.extractUserName(token);
+        }
+        // If Authentication Object is null Prepare Authentication Object
+        // from UsernamePasswordAuthenticationToken by passing UserDetails
+        if(userName!=null && SecurityContextHolder.getContext().getAuthentication()==null) {
+            UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(userName);
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
 
+        // forward the process from JwtFilter to UsernamePasswordAuthenticationFilter in Spring Security Servlet
+        filterChain.doFilter(request, response);
     }
 }
--- Security-Sample/src/main/java/com/security/security/service/JwtService.java
+++ Security-Sample/src/main/java/com/security/security/service/JwtService.java
@@ -1,15 +1,18 @@
 package com.security.security.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -19,7 +22,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*3))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*30))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -27,5 +30,34 @@ public class JwtService {
     private Key getKey() {
         byte[] keyBytes = Decoders.BASE64.decode("anFCNDAIUCNIUBAUVbisbdvwrbviubBIbvidBsDNviusdbIBV");
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build().parseClaimsJws(token).getBody();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date date = extractExpiration(token);
+        return date.before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
